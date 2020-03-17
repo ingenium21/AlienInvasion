0217728da7f653953b6f71b4f9addf50a8371335
Renato Regalado <renato.regalado@gmail.com>
2020-03-17 07:01:06 -0500

    added movement to the right and left

--- Modules/ship.py
+++ Modules/ship.py
@@ -8,12 +8,23 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        #movement flag
+        self.moving_right = False
+        self.moving_left = False
+
         # Load the ship(feelsCoolMan) image and gets its rect.
         self.image = pygame.image.load('images/feelsCoolMan.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+    def update(self):
+        """Update the ship's position based on the movement flags"""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
