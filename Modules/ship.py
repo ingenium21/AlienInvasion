aec73f7ac51d61bedf3047ead690e14c9de7cc46
Renato Regalado <renato.regalado@gmail.com>
2020-03-29 08:59:49 -0500

    shrunk the ship

--- Modules/ship.py
+++ Modules/ship.py
@@ -15,6 +15,7 @@ class Ship:
 
         # Load the ship(feelsCoolMan) image and gets its rect.
         self.image = pygame.image.load('images/feelsCoolMan.bmp')
+        self.image = pygame.transform.scale(self.image, (100,100))
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
